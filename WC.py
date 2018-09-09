ff3220a07fe655aec27e6b8cff544f24037cc541
conglinzhijiadebobo <43082731+conglinzhijiadebobo@users.noreply.github.com>
2018-09-09 17:55:47 +0800

    添加空行计数

--- WC.py
+++ WC.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from sys import argv
 
 script, filename = argv
@@ -5,18 +6,39 @@ script, filename = argv
 file = open(filename)
 str = file.read()
 
-def judgeword(c): 
+def judge_letter(c): 
     if c >= 'A' and c <= 'Z' or c >= 'a' and c <= 'z':
         return True
     else:
         return False
 		
+		
+def judge_visible_character(c):
+    if c >= '!' and c <= '~':
+        return True
+    else:
+        return False
 
 def code_line(l):
     if len(l) >= 2:
         return True
     else:
         return False
+		
+		
+def number_of_visible_characters_in_a_line(l):
+    number = 0
+    for char in l:
+        if judge_visible_character(char) == True:
+            number = number + 1
+    return number
+
+def blank_line(l):
+    if number_of_visible_characters_in_a_line(l) >= 2:
+        return False
+    else:
+        return True
+    
 
 def chara_count(s):
     return len(s)
@@ -26,10 +48,10 @@ def word_count(s):
     word_number = 0
     flag = 0
     for char in s:
-        if flag == 0 and judgeword(char) == True:
+        if flag == 0 and judge_letter(char) == True:
             flag = 1
             word_number = word_number + 1
-        if (flag == 1 and judgeword(char) == False) :
+        if (flag == 1 and judge_letter(char) == False) :
             flag = 0
     return word_number
 
@@ -43,19 +65,30 @@ def line_count(s):
         line_number = line_number + 1
     return line_number
 	
+	
+file.close()
 
-def lines_of_code_count(filename):
+
+file = open(filename)
+lines = file.readlines()
+	
+
+def lines_of_code_count(l):
     lines_of_code = 0
-    f = open(filename)
-    for line in f.readlines():
+    for line in l:
         if code_line(line) == True:
             lines_of_code = lines_of_code + 1
     return lines_of_code
 
 
-
-print lines_of_code_count(filename)	
-	
+def blank_lines_count(l):
+    number_of_blank_line = 0
+    for line in l:
+        if blank_line(line) == True:
+            number_of_blank_line = number_of_blank_line + 1
+    return number_of_blank_line
+    
+print blank_lines_count(lines)
 	
 	
 	
